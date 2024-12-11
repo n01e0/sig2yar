23aa24aa5159f5a6bb5549d585971b67623d1109
n01e0 <reoshiseki@gmail.com>
2024-12-11 22:57:39 +0900

    LogicalExpression

--- src/parser/logical/expression.rs
+++ src/parser/logical/expression.rs
@@ -1,96 +1,100 @@
 use nom::{
     branch::alt,
     bytes::complete::tag,
-    character::complete::digit1,
+    character::complete::{digit1, multispace0},
     combinator::{map, map_res, opt},
-    multi::many0,
-    sequence::{pair, separated_pair},
+    multi::separated_list0,
+    sequence::{delimited, preceded},
     IResult,
 };
-use anyhow::{Result, anyhow};
 
 #[derive(Debug, PartialEq)]
 pub enum LogicalExpression {
     SubExpression(usize),
     And(Vec<LogicalExpression>),
     Or(Vec<LogicalExpression>),
-    MatchCount(usize, usize),
-    MultiMatchCount(usize, usize, usize),
-    Gt(usize, usize),
-    MultiGt(usize, usize, usize),
-    Lt(usize, usize),
-    MultiLt(usize, usize, usize),
+    MatchCount(Box<LogicalExpression>, usize),
+    MultiMatchCount(Box<LogicalExpression>, usize, usize),
+    Gt(Box<LogicalExpression>, usize),
+    MultiGt(Box<LogicalExpression>, usize, usize),
+    Lt(Box<LogicalExpression>, usize),
+    MultiLt(Box<LogicalExpression>, usize, usize),
 }
 
 impl LogicalExpression {
-    pub fn parse(input: String) -> Result<LogicalExpression> {
+    pub fn parse(input: String) -> anyhow::Result<LogicalExpression> {
         match parse_expression(&input) {
             Ok(res) => Ok(res.1),
-            Err(e) => Err(anyhow!("Can't parse LogicalExpression: {}", e)),
+            Err(e) => Err(anyhow::anyhow!("Can't parse LogicalExpression: {}", e)),
         }
     }
 }
 
-fn parse_number(input: &str) -> IResult<&str, usize> {
+fn parse_number<'i>(input: &'i str) -> IResult<&'i str, usize> {
     map_res(digit1, str::parse::<usize>)(input)
 }
 
 fn parse_sub_expression<'i>(input: &'i str) -> IResult<&'i str, LogicalExpression> {
-    map(parse_number, LogicalExpression::SubExpression)(input)
-}
-
-fn parse_comparison<'i>(input: &'i str) -> IResult<&'i str, LogicalExpression> {
-    let (input, a) = parse_number(input)?;
-    let (input, op) = alt((tag("="), tag(">"), tag("<")))(input)?;
-    let (input, (x, y)) = separated_pair(parse_number, opt(tag(",")), opt(parse_number))(input)?;
-
-    match (op, y) {
-        ("=", Some(y_val)) => Ok((input, LogicalExpression::MultiMatchCount(a, x, y_val))),
-        ("=", None) => Ok((input, LogicalExpression::MatchCount(a, x))),
-        (">", Some(y_val)) => Ok((input, LogicalExpression::MultiGt(a, x, y_val))),
-        (">", None) => Ok((input, LogicalExpression::Gt(a, x))),
-        ("<", Some(y_val)) => Ok((input, LogicalExpression::MultiLt(a, x, y_val))),
-        ("<", None) => Ok((input, LogicalExpression::Lt(a, x))),
-        _ => unreachable!(),
-    }
-}
-
-
-fn parse_single_expression<'i>(input: &'i str) -> IResult<&'i str, LogicalExpression> {
     alt((
-        parse_comparison,
-        parse_sub_expression,
+        map(parse_number, LogicalExpression::SubExpression),
+        parse_paren_expression,
     ))(input)
 }
 
-fn parse_expression<'i>(input: &'i str) -> IResult<&'i str, LogicalExpression> {
-    let (input, mut initial) = parse_single_expression(input)?;
-    let (input, ops) = many0(pair(alt((tag("&"), tag("|"))), parse_single_expression))(input)?;
+fn parse_paren_expression<'i>(input: &'i str) -> IResult<&'i str, LogicalExpression> {
+    delimited(
+        preceded(multispace0, tag("(")),
+        parse_expression,
+        preceded(multispace0, tag(")")),
+    )(input)
+}
 
-    for (op, expr) in ops {
-        match op {
-            "&" => {
-                if let LogicalExpression::And(mut vec) = initial {
-                    vec.push(expr);
-                    initial = LogicalExpression::And(vec);
-                } else {
-                    initial = LogicalExpression::And(vec![initial, expr]);
-                }
-            }
-            "|" => {
-                if let LogicalExpression::Or(mut vec) = initial {
-                    vec.push(expr);
-                    initial = LogicalExpression::Or(vec);
-                } else {
-                    initial = LogicalExpression::Or(vec![initial, expr]);
-                }
-            }
+fn parse_comparison<'i>(input: &'i str) -> IResult<&'i str, LogicalExpression> {
+    let (input, expr) = parse_sub_expression(input)?;
+    let (input, op) = opt(alt((tag("="), tag(">"), tag("<"))))(input)?;
+    if let Some(op) = op {
+        let (input, x) = parse_number(input)?;
+        let (input, y) = opt(preceded(tag(","), parse_number))(input)?;
+        match (op, y) {
+            ("=", Some(y_val)) => Ok((input, LogicalExpression::MultiMatchCount(Box::new(expr), x, y_val))),
+            ("=", None) => Ok((input, LogicalExpression::MatchCount(Box::new(expr), x))),
+            (">", Some(y_val)) => Ok((input, LogicalExpression::MultiGt(Box::new(expr), x, y_val))),
+            (">", None) => Ok((input, LogicalExpression::Gt(Box::new(expr), x))),
+            ("<", Some(y_val)) => Ok((input, LogicalExpression::MultiLt(Box::new(expr), x, y_val))),
+            ("<", None) => Ok((input, LogicalExpression::Lt(Box::new(expr), x))),
             _ => unreachable!(),
         }
+    } else {
+        Ok((input, expr))
     }
-
-    Ok((input, initial))
 }
+
+fn parse_single_expression<'i>(input: &'i str) -> IResult<&'i str, LogicalExpression> {
+    parse_comparison(input)
+}
+
+fn parse_expression<'i>(input: &'i str) -> IResult<&'i str, LogicalExpression> {
+    parse_and_expression(input)
+}
+
+fn parse_and_expression<'i>(input: &'i str) -> IResult<&'i str, LogicalExpression> {
+    let (input, exprs) = separated_list0(preceded(multispace0, tag("&")), parse_or_expression)(input)?;
+    if exprs.len() == 1 {
+        Ok((input, exprs.into_iter().next().unwrap()))
+    } else {
+        Ok((input, LogicalExpression::And(exprs)))
+    }
+}
+
+fn parse_or_expression<'i>(input: &'i str) -> IResult<&'i str, LogicalExpression> {
+    let (input, exprs) = separated_list0(preceded(multispace0, tag("|")), parse_single_expression)(input)?;
+    if exprs.len() == 1 {
+        Ok((input, exprs.into_iter().next().unwrap()))
+    } else {
+        Ok((input, LogicalExpression::Or(exprs)))
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -99,7 +103,13 @@ mod tests {
         LogicalExpression::{
             SubExpression,
             And,
-            MultiMatchCount
+            Or,
+            MultiGt,
+            MultiLt,
+            MultiMatchCount,
+            Gt,
+            Lt,
+            MatchCount
         }
     };
 
@@ -111,10 +121,189 @@ mod tests {
     }
 
     #[test]
+    fn parse_only_or() {
+        let expression = "(0|1|2|3|4|5|6|7|8|9|10|11)";
+        let parsed = parse_expression(expression).unwrap();
+        assert_eq!(
+            Or((0..=11).map(|n| SubExpression(n)).collect()),
+            parsed.1
+        )
+    }
+
+    #[test]
     fn parse_multi_match() {
         let expression = "1=2,3";
         let parsed = parse_expression(expression).unwrap();
-        assert_eq!(MultiMatchCount(1,2,3), parsed.1);
+        assert_eq!(MultiMatchCount(Box::new(SubExpression(1)),2,3), parsed.1);
+
+        let expression = "(1&2)=2,2";
+        let parsed = parse_expression(expression).unwrap();
+        assert_eq!(MultiMatchCount(Box::new(And(vec![SubExpression(1), SubExpression(2)])),2,2), parsed.1);
+    }
+
+    #[test]
+    fn parse_and_or() {
+        let expression = "(0&1&2&3)&(4|1)";
+        let parsed = parse_expression(expression).unwrap();
+        assert_eq!(
+            And(
+                vec![
+                    And(
+                        vec![SubExpression(0), SubExpression(1), SubExpression(2), SubExpression(3)]
+                    ),
+                    Or(
+                        vec![SubExpression(4), SubExpression(1)]
+                    )
+                ]
+            ),
+        parsed.1)
+    }
+
+    #[test]
+    fn parse_gt() {
+        let expression = "(0>20)&1&2";
+        let parsed = parse_expression(expression).unwrap();
+        assert_eq!(
+            And(
+                vec![
+                    Gt(Box::new(SubExpression(0)), 20), SubExpression(1), SubExpression(2)
+                ]
+            ),
+            parsed.1
+        )
+    }
+
+    #[test]
+    fn parse_multi() {
+        let expression = "((0|1|2)>5,2)&(3|1)";
+        let parsed = parse_expression(expression).unwrap();
+        assert_eq!(
+            And(
+                vec![
+                    MultiGt(
+                        Box::new(Or(
+                            vec![SubExpression(0), SubExpression(1), SubExpression(2)]
+                        )),
+                        5,
+                        2
+                    ),
+                    Or(
+                        vec![SubExpression(3), SubExpression(1)]
+                    )
+                ],
+            ),
+            parsed.1
+        );
+        let expression = "((0|1|2)<5,2)&(3|1)";
+        let parsed = parse_expression(expression).unwrap();
+        assert_eq!(
+            And(
+                vec![
+                    MultiLt(
+                        Box::new(Or(
+                            vec![SubExpression(0), SubExpression(1), SubExpression(2)]
+                        )),
+                        5,
+                        2
+                    ),
+                    Or(
+                        vec![SubExpression(3), SubExpression(1)]
+                    )
+                ],
+            ),
+            parsed.1
+        )
+    }
+
+    #[test]
+    fn parse_count() {
+        let expression = "((0|1|2|3)=2)&(4|1)";
+        let parsed = parse_expression(expression).unwrap();
+        assert_eq!(
+            And(
+                vec![
+                    MatchCount(
+                        Box::new(
+                            Or(
+                                vec![SubExpression(0), SubExpression(1), SubExpression(2), SubExpression(3)]
+                            )
+                        ),
+                        2
+                    ),
+                    Or(
+                        vec![SubExpression(4), SubExpression(1)]
+                    )
+                ]
+            ),
+            parsed.1
+        );
+    }
+
+    #[test]
+    fn parse_and() {
+        let expression = "((0|1)&(2|3))&4";
+        let parsed = parse_expression(expression).unwrap();
+        assert_eq!(
+            And(vec![
+                And(vec![
+                    Or(vec![SubExpression(0), SubExpression(1)]),
+                    Or(vec![SubExpression(2), SubExpression(3)])
+                ]),
+                SubExpression(4)
+            ]),
+            parsed.1
+        );
+
+        let expression = "0>2&1";
+        let parsed = parse_expression(expression).unwrap();
+        assert_eq!(
+            And(vec![
+                Gt(Box::new(SubExpression(0)), 2),
+                SubExpression(1)
+            ]),
+            parsed.1
+        )
+    }
+
+
+    #[test]
+    fn parse_doc_trojan() {
+        let expression = "0&1>50";
+        let parsed = parse_expression(expression).unwrap();
+        assert_eq!(
+            And(
+                vec![SubExpression(0), Gt(Box::new(SubExpression(1)), 50)],
+            ),
+            parsed.1
+        )
+    }
+
+    #[test]
+    fn parse_obfus_macro() {
+        let expression = "0&1>200&2<1000&3>50&4>200";
+        let parsed = parse_expression(expression).unwrap();
+        assert_eq!(
+            And(vec![
+                SubExpression(0),
+                Gt(
+                    Box::new(SubExpression(1)),
+                    200
+                ),
+                Lt(
+                    Box::new(SubExpression(2)),
+                    1000
+                ),
+                Gt(
+                    Box::new(SubExpression(3)),
+                    50
+                ),
+                Gt(
+                    Box::new(SubExpression(4)),
+                    200
+                )
+            ]),
+            parsed.1
+        )
     }
 }
 
