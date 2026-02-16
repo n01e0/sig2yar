0f3f6ef28226ba11b0fc85ceb8716e7d396f1530
n01e0 <47088622+n01e0@users.noreply.github.com>
2026-02-16 19:00:29 +0900

    test: lock pcre trigger empty-middle offset strict-false (#210)

--- tests/yara_compile.rs
+++ tests/yara_compile.rs
@@ -327,6 +327,16 @@ fn yara_rule_with_pcre_trigger_prefix_extra_comma_offset_false_rejects_scan() {
 }
 
 #[test]
+fn yara_rule_with_pcre_trigger_prefix_empty_middle_offset_token_false_rejects_scan() {
+    let sig = LogicalSignature::parse("Foo.Bar-1;Target:1;1;41414141;200,,300:0/abc/").unwrap();
+    let rule = YaraRule::try_from(&sig).unwrap();
+    let src = rule.to_string();
+
+    assert!(src.contains("pcre offset prefix '200,,300' unsupported"));
+    assert_eq!(scan_match_count(src.as_str(), b"AAAAabc"), 0);
+}
+
+#[test]
 fn yara_rule_with_pcre_trigger_prefix_mixed_missing_reference_false_rejects_scan() {
     let sig = LogicalSignature::parse("Foo.Bar-1;Target:1;1;41414141;0|9/abc/").unwrap();
     let rule = YaraRule::try_from(&sig).unwrap();
